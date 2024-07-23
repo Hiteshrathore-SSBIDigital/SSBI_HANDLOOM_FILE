cf1ebfd031a9a7cda2bb6ef48c79a678384140cd
hiteshrathore446 <hvr446@gmail.com>
2024-07-23 17:40:45 +0530

    change

---
+++ Notification.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:nehhdc_app/Setting_Screen/Setting_Screen.dart';
+
+class Notification_Screen extends StatefulWidget {
+  const Notification_Screen({super.key});
+
+  @override
+  State<Notification_Screen> createState() => _Notification_ScreenState();
+}
+
+class _Notification_ScreenState extends State<Notification_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Notification",
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Color(ColorVal),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
